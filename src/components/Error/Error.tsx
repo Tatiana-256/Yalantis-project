6a109b62a7ad63fd1b4e27a51468f2289cc0ec0f
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2021-01-06 10:53:17 +0200

    Remove Error.module.css.

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import {StyledLink} from '../Navigation/Navigation-styles';
-import style from './Error.module.css';
-
+import React from "react";
+import { StyledLink } from "../Navigation/Navigation-styles";
+import { Button, Text, Wrapper } from "./Error-style";
 
 export const Error404 = () => {
-
-    return <div className={style.error_wrapper}>
-<div className={style.error_text}>        Sorry, this page doesn`t exist</div>
-        <div className={style.error_button}><StyledLink to="/products">Go to main page</StyledLink></div>
-    </div>
-}
+  return (
+    <Wrapper>
+      <Text> Sorry, this page doesn`t exist</Text>
+      <Button>
+        <StyledLink to="/products">Go to main page</StyledLink>
+      </Button>
+    </Wrapper>
+  );
+};
