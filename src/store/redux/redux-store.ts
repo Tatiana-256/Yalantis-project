b99ae032027a26f8949d21019ba20299b8b711ee
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2021-02-06 22:45:20 +0200

    small fix redux-store.ts

--- src/store/redux/redux-store.ts
+++ src/store/redux/redux-store.ts
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./slices/prosuctSlice";
-import filterSlise from "./slices/filterSlice";
+import filterSlice from "./slices/filterSlice";
 import { uiReducer } from "./UI-handling/ui-reduser";
 import ordersSlice from "./slices/ordersSlice";
 
 const store = configureStore({
   reducer: {
     products: productReducer,
-    filter: filterSlise,
+    filter: filterSlice,
     orders: ordersSlice,
     ui: uiReducer,
   },
